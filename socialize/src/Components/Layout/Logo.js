f7b4c150417c2c0708868103f731174f8ed39949
Minyoi87 <59666333+Minyoi87@users.noreply.github.com>
2020-08-10 13:34:52 +0300

    creating the navigation bar with materialize

---
+++ socialize/src/Components/Layout/Logo.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+
+
+
+
+
+const Logo = ()=> {
+    return (
+        <div className ="">
+            <a href="#" className="brand-logo">Logo</a>
+
+
+            
+        </div>
+    )
+}
+
+export default Logo;
---
+++ socialize/src/Components/Layout/NavBar.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import Logo from "../Layout/Logo"
+
+export default class NavBar extends Component {
+    render() {
+        return (
+            <nav>
+              <div class="nav-wrapper">
+                 <Logo/>
+      
+            
+              </div>
+            </nav>
+
+            
+            
+        )
+    }
+}
